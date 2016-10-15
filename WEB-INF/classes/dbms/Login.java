b1e8e86d067079bfe06195571c4ec6d6bb9dd3c8
Anubhav Jain <Anubhav Jain>
2016-10-15 15:19:11 +0530

    Servlets for register and login page updated

---
+++ WEB-INF/classes/dbms/Login.java
@@ -0,0 +1,42 @@
+package dbms;
+
+import javax.servlet.http.*;
+import java.sql.*;
+import java.io.*;
+
+import dbms.DBInfo.*;
+
+public class Login extends HttpServlet{
+	public void doPost(HttpServletRequest req,HttpServletResponse res)throws IOException{
+		String s1 = req.getParameter("username");
+		String s2 = req.getParameter("pass");
+		
+		String query = "select * from users where email=? and password=?";
+		Connection con = DBInfo.con;
+		PrintWriter pw = res.getWriter();
+
+		int flag = 0;
+		try{
+			PreparedStatement ps = con.prepareStatement(query);
+			ps.setString(1,s1);
+			ps.setString(2,s2);
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()){
+				flag = 1;
+				break;
+			}
+		}
+		catch(Exception e){ e.printStackTrace();}
+		
+		if(flag==1){
+			pw.println("<html>");
+			pw.println("<body>");				
+			pw.println("<script>");
+			pw.println("alert('Login successful')");
+			pw.println("</script>");
+			pw.println("</body></html>");
+		}
+		else
+			res.sendRedirect("../login.html");	
+	}
+}
